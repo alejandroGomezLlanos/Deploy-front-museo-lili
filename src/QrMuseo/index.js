b7403ed5732059ef7d8c8518117a4d1566e3abbd
Luis Enrique Sierra Salazar <luisesierras1907@gmail.com>
2024-07-24 22:11:42 -0500

    Función put para cambiar el codigo a nuestro deseo

--- src/QrMuseo/index.js
+++ src/QrMuseo/index.js
@@ -17,11 +17,15 @@ function QrMuseo() {
   const fetchRoomData = async () => {
     try {
       // Obtener el código de la sala
-      const roomResponse = await axios.get("https://testdeploy-production-9d97.up.railway.app/roomCode");
+      const roomResponse = await axios.get(
+        "https://testdeploy-production-9d97.up.railway.app/roomCode"
+      );
       setRoomCode(roomResponse.data[0].code);
 
       // Obtener el recuento de usuarios
-      const userResponse = await axios.get("https://testdeploy-production-9d97.up.railway.app/users");
+      const userResponse = await axios.get(
+        "https://testdeploy-production-9d97.up.railway.app/users"
+      );
       const users = userResponse.data;
       const count = users.filter((user) => user.codigoSala === roomCode).length;
       setUserCount(count);
@@ -49,7 +53,6 @@ function QrMuseo() {
     };
   }, [roomCode, userCount]);
 
-
   // Redireccionar a /tematicaMuseo cuando la sala está llena
   useEffect(() => {
     if (isRoomFull) {
@@ -57,8 +60,50 @@ function QrMuseo() {
     }
   }, [isRoomFull, navigate]);
 
+  // Función para generar un código de sala aleatorio de 4 caracteres
+  const generateRoomCode = () => {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let code = "";
+    for (let i = 0; i < 4; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      code += characters[randomIndex];
+    }
+    return code;
+  };
+
+  // Función para actualizar o crear un nuevo código de sala en la base de datos
+  const updateRoomCode = async () => {
+    try {
+      const newCode = generateRoomCode();
+      const response = await axios.put(
+        "https://testdeploy-production-9d97.up.railway.app/roomcode",
+        {
+          code: newCode,
+          huaqueroSymbols: []
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log(`Room code updated to: ${newCode}`);
+    } catch (error) {
+      console.error("Error updating room code:", error);
+    }
+  };
+
+  // Llamar a la función para actualizar el código de sala al cargar la página
+  useEffect(() => {
+    updateRoomCode();
+  }, []);
+
   return (
-    <div className="container-qr-museo" style={{ backgroundImage: `url(${fondo})` }}>
+    <div
+      className="container-qr-museo"
+      style={{ backgroundImage: `url(${fondo})` }}
+    >
       <div className="texto-contenedor">
         <div className="texto-informacion">
           <div className="linea-texto">Lee el código QR e ingresa</div>
